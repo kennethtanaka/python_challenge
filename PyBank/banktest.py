cf62dfdc0faa59c56dd1323e5ecd83edc910a82f
kenneth tanaka <kennethtanaka@msn.com>
2019-12-02 14:18:37 -0800

    submission

--- PyBank/banktest.py
+++
@@ -1,7 +0,0 @@
-# Define the function and have it accept the 'data' as its sole parameter
-def greatest(data):
-    # For readability, it can help to assign your values to variables with descriptive names
-    date = str(data[0])
-    high = int(data[1])
-# Print out the date and greatest
-    print(f"Greatest Increse for profits {date} {high}")

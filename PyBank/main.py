5ea8fdcdd8b21a9c179fad6d27f82d74c1f68b04
kenneth tanaka <kennethtanaka@msn.com>
2019-12-06 09:03:43 -0800

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -36,9 +36,18 @@ with open(bank_csv, 'r') as csvfile:
         total = total + (int(row[1]))
                
         #create a variable that will count the revenue change
-        delta = int(row[1]) - num1
-        num1 = int(row[1])
-        totaldelta = delta + totaldelta    
+        delta = int(row[1]) - num2
+
+        #if will check to ensure first delta is not included in average of totaldelta   
+        #totaldelta is net total amount of "Profit/Losses" over the entire period   
+        if delta == int(row[1]):
+            totaldelta = 0
+        else:
+            totaldelta = delta + totaldelta 
+
+        #make num2 = num1 to be able to calc delta
+        num2 = int(row[1]) 
+
         #find greatest increase/decrease profit change and date                                  
         if delta > maxdelta:
             maxdelta = delta
@@ -46,12 +55,12 @@ with open(bank_csv, 'r') as csvfile:
         if delta < mindelta:
             mindelta = delta
             mindeltadate = row[0]
-        
-    num2 = num1
 
-    #calculate the average change in revenue
-    avgdelta = round(totaldelta/((count)-1))
-                
+    #calculate the average of the change of "Profit/Losses" over the entire period
+    avgdelta = round(totaldelta/((count)-1),2)
+         
+    print (f"Financial Analysis") 
+    print ('------------------------')             
     print (f"Total months: {count}")    
     print (f"Total: ${total}")
     print (f"Average Change: {avgdelta}")
@@ -60,6 +69,8 @@ with open(bank_csv, 'r') as csvfile:
 
 #Output final results to txt file
 with open("pybank.txt","w") as txtfile:
+    txtfile.write(f"Financial Analysis\n") 
+    txtfile.write('-------------------\n')
     txtfile.write(f"Total months: {count}\n")
     txtfile.write(f"Total: ${total} \n")
     txtfile.write(f"Average Change: ${avgdelta}\n")
